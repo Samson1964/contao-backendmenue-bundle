b6360a8c7bbae2fc4bc1650242e4c73319dc71f9
Samson Hoppe <hoppe.berlin@googlemail.com>
2026-08-10 05:17:24 +0200

    Add German translation for backend module

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['MOD']['backendmenue_bereiche'] = [
+    'Eigenes Backend-Menü',
+    'Verwalte benutzerdefinierte Menübereiche und ordne Backend-Module um'
+];
