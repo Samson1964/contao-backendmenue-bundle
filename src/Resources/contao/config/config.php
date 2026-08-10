a90b7094401773e46eb5902d766f6929276c49f8
Samson Hoppe <hoppe.berlin@googlemail.com>
2026-08-10 05:58:57 +0200

    Fix: Remove manual hook registration causing DI errors
    - Remove loadDataContainer hook from config.php (conflicts with #[AsHook])
    - Remove unused InitializeSystemListener
    - Let Symfony's hook attribute system handle registration

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -8,8 +8,3 @@ declare(strict_types=1);
 $GLOBALS['BE_MOD']['system']['backendmenue_bereiche'] = [
     'tables' => ['tl_backendmenue_bereiche', 'tl_backendmenue_zuordnungen'],
 ];
-
-/**
- * Hook zum Laden der DCA-Dateien.
- */
-$GLOBALS['TL_HOOKS']['loadDataContainer'][] = ['Schachbulle\BackendMenueBundle\EventListener\LoadDataContainerListener', '__invoke'];
